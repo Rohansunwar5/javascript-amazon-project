1f355b75209d5b797726da3d8d57fabe8bbd242c
Amazon Clone <rohansunwar5@gmail.com>
2023-11-26 20:19:54 +0530

    module addition

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,6 @@
 // we are gonna use accumulator pattern: it is the pattern in which we loop through an array and add it on the same variable
+import { cart } from "../data/cart.js";
+// we can name the imported variable as another name too using import {cart as myCart}
 
 let productsHTML = "";
 
