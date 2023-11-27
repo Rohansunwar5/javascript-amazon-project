a47ba46e7fc46805244bc0d8a75b8c31cac5ba84
Amazon Clone <rohansunwar5@gmail.com>
2023-11-27 22:59:30 +0530

    delivery options

---
+++ data/deliveryOptions.js
@@ -0,0 +1,17 @@
+export const deliveryOptions = [
+  {
+    id: "1",
+    deilveryDays: 7,
+    priceCents: 0,
+  },
+  {
+    id: "2",
+    deilveryDays: 3,
+    priceCents: 499,
+  },
+  {
+    id: "3",
+    deilveryDays: 1,
+    priceCents: 999,
+  },
+];
