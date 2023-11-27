8ff795cd3fa5f6e151ed21434af6038b65f3f1a6
Amazon Clone <rohansunwar5@gmail.com>
2023-11-27 19:33:12 +0530

    delete products from html when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,7 +16,7 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
   <div class="delivery-date">
     Delivery date: Tuesday, June 21
   </div>
@@ -113,6 +113,9 @@ document.querySelectorAll(".js-delete-product").forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
+    const TobeDeleted = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    TobeDeleted.remove();
   });
 });
